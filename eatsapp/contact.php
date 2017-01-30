d6a534c5f013f589b52c29a746a6cf05c5f13f19
hemu0551 <hemanthkandregula@gmail.com>
2017-01-30 12:13:56 +0530

    eatsapp.in

---
+++ eatsapp/contact.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+
+
+<div class="container container1">
+	<div class="well">
+	<h1 id="center">Contact Us</h1>
+	</div>
+	<div class="container col-lg-3">
+		<ul class="liststyle">
+        <li class="liststyle1"><b>Contact Us:</b></li>
+        <li class="liststyle1">Free Press Journal Marg,</li>
+        <li class="liststyle1">B wing, Nariman Point,</li>
+        <li class="liststyle1">Mumbai 400021</li>   
+        <li class="liststyle1"><a href="#"><u>contact@wolotech.com</u></a></li><br> 
+      </ul>
+ 	</div>
+  <div class="col-lg-9">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d471.7670436221902!2d72.823486!3d18.925358!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7cba706a705a12b7!2sMittal+Court!5e0!3m2!1sen!2sus!4v1485261676056" width="900" height="500" frameborder="0" style="border:0" class="screen" allowfullscreen></iframe>
+  </div>
+</div>
+<div style="height: 1.7em;"></div>
+
+<?php include 'footer.php'; ?>
---
+++ eatsapp/index.php
@@ -0,0 +1,36 @@
+<?php include 'header.php'; ?>
+
+<body class="back123">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Eatsapp</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="terms.php">Terms of Service</a></li>
+            <li><a href="policy.php">Privacy Policy</a></li>
+            <li><a href="refund.php">Cancellation and Refunds Policy</a></li>
+            <li><a href="disclaim.php">Disclaimers Identification</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="back">
+      <div class="container-fluid position">
+        <h1 class="position1">Eatsapp</h1>
+      </div>
+    </div>
+    <p class="position2">(formerly grazzy)</p>
+    <div class="height1"></div>
+
+<?php include 'footer.php'; ?>
