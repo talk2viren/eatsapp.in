7b1b11384d6734e059a18bc5a0ed0501beb9ded4
hemu0551 <hemanthkandregula@gmail.com>
2017-04-23 18:09:04 +0530

    delete

--- eatsapp/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="black col-lg-12">
-	<div class="container"><a href="https://facebook.com/eatsapp.in" target="_blank"><i class="fa fa-facebook" aria-hidden="true" id="fontsize"></i></a><a href="https://www.instagram.com/Eatsapp.in/" target="_blank"><i class="fa fa-instagram" aria-hidden="true" id="fontsize"></i></a></div>
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
